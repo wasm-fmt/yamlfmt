a229f3284db5328022d2ddc436259c526556da14
magic-akari <akari.ccino@gmail.com>
2024-08-15 13:26:57 +0800

    chore: Update tests

--- test_node/test-node.mjs
+++ test_node/test-node.mjs
@@ -1,42 +1,30 @@
-import init, { format } from "../pkg/yamlfmt_node.js";
-import { test } from "node:test";
 import assert from "node:assert/strict";
 import fs from "node:fs/promises";
 import path from "node:path";
+import { test } from "node:test";
+import init, { format } from "../pkg/yamlfmt_node.js";
 import { fileURLToPath } from "node:url";
 
 await init();
 
 const test_root = fileURLToPath(new URL("../test_data", import.meta.url));
 
-for await (const dirent of await fs.opendir(test_root, { recursive: true })) {
-	if (!dirent.isFile()) {
+for await (const test_file of fs.glob("**/*.yaml", {
+	cwd: test_root,
+})) {
+	if (test_file.startsWith(".")) {
 		continue;
 	}
 
-	if (dirent.name.startsWith(".")) {
-		continue;
-	}
+	const input_path = path.join(test_root, test_file);
 
-	const input_path = path.join(dirent.path, dirent.name);
-	const ext = path.extname(input_path);
-
-	switch (ext) {
-		case ".yaml":
-			break;
-
-		default:
-			continue;
-	}
-
-	const test_name = path.relative(test_root, input_path);
 	const [input, expected] = await Promise.all([
 		fs.readFile(input_path, { encoding: "utf-8" }),
 		fs.readFile(input_path + ".snap", { encoding: "utf-8" }),
 	]);
 
-	test(test_name, () => {
-        const actual = format(input, input_path);
+	test(test_file, () => {
+		const actual = format(input, test_file);
 		assert.equal(actual, expected);
 	});
 }
